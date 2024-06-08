dbee44a7e03a59d462fad8bfb603793276bd1657
Md_Humayun_Kabir <kabir@revo-interactive.com>
2024-06-08 21:49:15 +0600

    first commit

---
+++ src/main/java/com/doctorschember/DoctorsChemberApplication.java
@@ -0,0 +1,13 @@
+package com.doctorschember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsChemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsChemberApplication.class, args);
+	}
+
+}
